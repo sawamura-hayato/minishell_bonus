192896a9a82808edc43a1d41640936e57f6cb0eb
Tatsuki Terao <tterao@c1r25s3.42tokyo.jp>
2023-07-08 20:18:55 +0900

    設計：expansion修正

--- includes/expansion.h
+++ includes/expansion.h
@@ -6,7 +6,7 @@
 /*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 15:49:23 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/07/08 18:22:31 by tterao           ###   ########.fr       */
+/*   Updated: 2023/07/08 20:16:16 by tterao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,42 @@ void		expand_word_splitting(t_ast *node);//IFSを確認 クオテーションで
 void		expand_filename(t_ast *node);//＊の展開 + 線形リストのトークンを追加
 void		expand_quotation(t_ast *node);//クオテーションの削除
 char		*get_env_x(char *target, const t_env **envs);
-t_command	*command_split_node(t_command **head, t_command *where, t_command *newnode);//word splittingでノードを追加
-t_redirect	*redirect_add_node(t_redirect **head, t_redirect *where, t_redirect *newnode);//word splittingでノードを追加
+t_command	*command_split_node(t_command *node);//word splittingでノードを分割
+t_redirect	*redirect_add_node(t_redirect *node);//word splittingでノードを分割
 
 //heredoc
 void		do_heredoc(t_ast *node);//heredocを実行し、文字列を取得
 void		expand_heredoc(t_ast *node, const t_envs **envs);//delimiterにクオテーションがない場合、環境変数を展開
 
-int	expansion(t_ast *node)
+
+t_command	*command_split_node(t_command *node)
+{
+
+	t_command	*next = node->next;
+	//一番最初はnode->wordだけ変更する
+	if (is_ifs(node->word, try_get_env("IFS", envs)))
+	{
+		//[testa   b   c+test] -> [testa] [b   c+test]
+		char	*tmp = node->word;
+		node->word = try_substr(node->word, 0, strchr(node->word, try_get_env("IFS", envs)) - node->word[0]);
+		t_command	*newnode = try_strdup(strchr(node->word, try_get_env("IFS", envs)));
+		free(tmp);
+	}
+	//ifs区切り文字がある限りloop
+	//[testa] [b   c  test] -> [b] [c+test]
+	//final result = [a] [b] [c+test]
+	while (is_ifs(node->word, try_get_env("IFS", envs)))
+	{
+		//ifs区切り文字がwordにあった場合、その文字アドレスを取得
+		//command_newnodeの中で、strchrの返り値のアドレスをstrdupする
+		t_command	*tmp = node;
+		newnode = command_newnode(strchr(is_ifs(node->word, try_get_env("IFS", envs))));
+		tmp->next = newnode;
+	}
+	newnode->next = next;
+}
+
+int	expansion(t_ast *node, t_envs **envs)
 {
 	int	ret;
 	if (node->left_hand != NULL)
@@ -39,7 +67,7 @@ int	expansion(t_ast *node)
 	if (ret)
 		return;
 	expand_variable(node);
-	expand_word_splitting(node);
+	expand_word_splitting(node, envs);
 	expand_filename(node);
 	expand_quotation(node);
 	if (do_heredoc(node))
