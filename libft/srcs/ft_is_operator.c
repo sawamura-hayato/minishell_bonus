23366caeec4f7bdea844161aeb3be4da80db1ab5
Sawa <sunzecun@gmail.com>
2023-07-26 17:01:29 +0900

    |,&の制御演算子を判定する関数

--- libft/srcs/ft_is_operator.c
+++ libft/srcs/ft_is_operator.c
@@ -1,24 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isoperator.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hsawamur <hsawamur@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/24 14:52:38 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/07/26 17:00:26 by hsawamur         ###   ########.fr       */
+/*   Created: 2023/07/26 16:52:20 by hsawamur          #+#    #+#             */
+/*   Updated: 2023/07/26 16:58:04 by hsawamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-#include <stdlib.h>
 #include <stdbool.h>
 
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *str);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strtrim(char const *s1, char const *set);
-bool	ft_is_operator(char c);
-#endif
+#define LOGICAL_OR '|'
+#define LOGICAL_AND '&'
+
+bool	ft_is_operator(char c)
+{
+	if (c == LOGICAL_OR || c == LOGICAL_AND)
+		return (true);
+	return (false);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	if (ft_is_operator('l'))
+// 		printf("ok\n");
+// 	if (ft_is_operator('d'))
+// 		printf("ok\n");
+// 	else
+// 		printf("ko\n");
+// 	return (0);
+// }
