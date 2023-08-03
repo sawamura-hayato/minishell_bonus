cfd2b8ccfc49b59b15106ed86edaac926702bcaf
Tatsuki Terao <tterao@c1r24s1.42tokyo.jp>
2023-08-03 17:08:35 +0900

    【設計】init.h

--- includes/init.h
+++ includes/init.h
@@ -6,7 +6,7 @@
 /*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 15:15:31 by tterao            #+#    #+#             */
-/*   Updated: 2023/08/01 13:13:34 by tterao           ###   ########.fr       */
+/*   Updated: 2023/08/03 16:57:34 by tterao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,20 @@ typedef struct s_envs
 
 typedef struct s_data
 {
-	t_envs	*envs;
+	t_envs	**envs_hashmap;
 	int		exit_status;
 }	t_data;
 
-
-
-t_envs	*envs_init(const char **environ);
-t_envs	*envs_newnode(char *_key, char *_value);
-void	envs_addback(t_envs **head);
+void	envs_init(const char **environ, t_data d);
+t_envs	*envs_newnode(char *_key, char *_value, t_envs **envs_hashmap);
+void	*envs_modify(char *_key, char *new_value, t_envs **envs_hashmap);
+void	*envs_addstr(char *_key, char *adding_value, t_envs **envs_hashmap);
+void	*envs_delete(char *_key, t_envs **envs_hashmap);
+bool	is_top(char alpha, char *_key);
+t_envs	*get_node(char *_key);
+int		get_hashmap_index(char alpha);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_isupper(char c);
+int		ft_islower(char c);
 
 #endif
