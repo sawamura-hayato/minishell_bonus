d9deacdc3145d7addd135797a2d9672c9928b620
Sawa <sunzecun@gmail.com>
2023-08-08 17:55:21 +0900

    単語の分割のメンバ変数、関数作成

--- srcs/expansion/expand_word_splitting.c
+++ srcs/expansion/expand_word_splitting.c
@@ -6,28 +6,88 @@
 /*   By: hsawamur <hsawamur@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 16:30:36 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/08/08 16:44:02 by hsawamur         ###   ########.fr       */
+/*   Updated: 2023/08/08 17:54:42 by hsawamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expansion.h"
 
-void	expand_splitting_word_list(t_word_list *word_list, t_envs *ifs)
+char *expand_get_splitting_word(char **word)
 {
 	
+	while (*word)
+	{
+		while(ft_isspace(*word))
+			word++;
+		
+	}
+	return (word);
 }
 
-void	expand_splitting_redirect_list(t_redirect *redirect_list, t_envs *ifs)
+void expand_splitting_word_list(t_word_list **word_list)
 {
-	
+	t_word_list *new_word_list;
+	char *tmp_word;
+
+	tmp_word = (*word_list)->word;
+	(*word_list)->word = expand_get_splitting_word(&tmp_word);
+	while (*tmp_word)
+	{
+		new_word_list = ast_init_word_node(expand_get_splitting_word(&tmp_word));
+		(*word_list)->next = new_word_list;
+		word_list = (*word_list)->next;
+	}
+	free(tmp_word);
 }
 
-void	expand_word_splitting(t_ast *node, t_data *d)
+void expand_splitting_word_list(t_word_list *word_list, t_envs *ifs)
 {
-	t_envs	*ifs;
+	t_word_list *tmp_word_list;
+	t_word_list *new_word_list;
+	t_word_list	*head;
 
-	ifs = get_target_value(d->envs_hashmap, "IFS");
-	expand_splitting_word_list(node->command_list->word_list, ifs);
-	expand_splitting_redirect_list(node->command_list->word_list, ifs);
-	
+	tmp_word_list = NULL;
+	new_word_list = NULL;
+	head = word_list;
+	// IFSがデフォルトの場合
+	if (expand_is_ifs_default(ifs->value) || ft_strchr(ifs->value, ' '))
+	{
+		// 通常の変数展開後の場合空白、タブ、改行があったらトークンを分ける。
+		while (word_list != NULL)
+		{
+			if (word_list->type == SINGLE_QUOTE || word_list->type == DOUBLE_QUOTE)
+			{
+				word_list = word_list->next;
+				while (word_list->type == SINGLE_QUOTE || word_list->type == DOUBLE_QUOTE)
+					word_list = word_list->next;
+			}
+			if (expand_is_check_space_word(word_list))
+			{
+				tmp_word_list = word_list->next;
+				expand_splitting_word_list(&word_list);
+				word_list->next = tmp_word_list;
+			}
+		}
+	}
+	// IFSが任意で設定されている場合
+	word_list = head;
+	while (word_list != NULL)
+	{
+		//任意の文字があった場合空白にする
+		expand_blank_word_if_ifs_char(word_list, ifs->value);
+		word_list = word_list->next;
+	}
+}
+
+void expand_splitting_redirect_list(t_redirect *redirect_list, t_envs *ifs)
+{
+}
+
+void expand_word_splitting(t_ast *node, t_data *d)
+{
+	t_envs *ifs;
+
+	ifs = ft_get_env_target_value(d->envs_hashmap, "IFS");
+	expand_splitting_word_list(&node->command_list->word_list, ifs);
+	expand_splitting_redirect_list(&(node->command_list->redirect_list), ifs);
 }
