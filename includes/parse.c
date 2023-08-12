cd29ad6d7ebffe08a6dab24bd4b817a129892465
taka-chin <ty80164@gmail.com>
2023-08-12 13:32:38 +0900

    指摘事項反映　途中

--- includes/parse.c
+++ includes/parse.c
@@ -6,182 +6,199 @@
 /*   By: tyamauch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 21:44:42 by tyamauch          #+#    #+#             */
-/*   Updated: 2023/08/10 20:03:38 by tyamauch         ###   ########.fr       */
+/*   Updated: 2023/08/12 03:31:32 by tyamauch         ###   ########.fr       */
 /* ************************************************************************** */
 
 /*                                                                            */
 #include "parse.h"
+#include "tokenize.h"
 
-t_ast *parse(t_token **current_token, t_data *d) 
+t_ast	*parse(t_token **current_token, t_data *d)
 {
-	t_token *token = *current_token;
- 	t_ast *left_node = ast_command(token,d);
-	t_ast_node_type type;
+	t_token			*token;
+	t_ast			*left_node;
+	t_ast_node_type	type;
 
-	if(left_node == NULL)
+	token = *current_token;
+	left_node = ast_command_node(token, d);
+	if (left_node == null)
 		ast_free_all_nodes(left_node);
-  	while (true)
-  	{
-    	if (token != NULL && ast_is_operator(token->type))
-			{
-				type = token -> type;
-				token = token ->next;//operatarのtoken
-      	left_node = ast_operator(type, left_node, ast_command(token,d));
-	  		token = token ->next;// 
-				}
-    		else
-      		return left_node;
+	while (true)
+	{
+		if (token != null && ast_is_operator(token->type))
+		{
+			type = token->type;
+			token = token->next; //operatarのtoken
+			left_node = ast_operator_node(type, left_node,
+					ast_command_node(token, d));
+			token = token->next; //
 		}
+		else
+			return (left_node);
+	}
 }
 
-t_ast *ast_command(t_token **current_token,t_data *d) 
+t_ast	*ast_command_node(t_token **current_token, t_data *d)
 {
-	t_ast *command_node;
-	t_token *token = *current_token; 	
-  if (token->word[0] == '(') 
-  {
-    t_ast *node = parse(current_token,d);
-    expect(token,')');
-    return node;
-  }
-  if (token == null || ast_is_opereter(token))
+	t_ast	*ast_command_node;
+	t_token	*token;
+	t_ast	*node;
+
+	ast_command_node = ast_init_node();
+	ast_command_node->type = COMMAND;
+	token = *current_token;
+	if (token->word[0] == '(')
+	{
+		node = parse(current_token, d);
+		expect(*token, ')');
+		return (node);
+	}
+	if (token == null || ast_is_opereter(token->type))
 		ast_syntax_error(d);
-  return ast_command_list(command_node,current_token); 
+	return (ast_command_list(ast_command_node, current_token));
 }
 
-t_ast * ast_command_list(t_ast *node,t_token **current_token)
+t_ast	*ast_command_list(t_ast *ast_command_node, t_token **current_token)
 {
 	token = *current_token;
-
-	while(token != NULL && !ast_is_opereter(token -> type) )
+	while (token != NULL && !ast_is_opereter(token->type))
 	{
-		node->command_list->fd = STDOUT_FINENO;
-		node->command_list->pid = -1;
-		if(ast_is_redirect(token))
-			ridirect_list(&(node -> redirect_list),current_token);
+		ast_command_node->command_list->fd = STDOUT_FINENO;
+		ast_command_node->command_list->pid = -1;
+		if (command_is_redirect(token->type))
+			command_ridirect_list(&(command_node->command_list->redirect_list),
+									current_token);
 		else
-			command_word_list(&(node -> command_list),current_token);
+			command_word_list(&(command_node->command_list->word_list),
+								current_token);
 		if (!token_next(&current_token))
 			return (NULL);
 	}
-	return(node);
+	return (command_node);
 }
 
-t_ast * ast_operator(e_ast_type type,t_ast *left_hand,t_ast *right_hand )
+t_ast	*ast_operator_node(e_ast_type type, t_ast *left_hand, t_ast *right_hand)
 {
-	t_ast * ast_ope;
+	t_ast	*ast_operator_node;
 
-	if(right_hand== NULL)
-		return(syntax_error(d));//left_hand free
-	ast_ope = try_calloc(1,sizeof(t_ast));
-	ast_ope -> type = type;
-	ast_ope -> left_hand = left_hand;
-	ast_ope -> right_hand = right_hand;
-	return (ast_ope);
+	if (right_hand == NULL)
+		return (syntax_error(d)); //left_hand free
+	ast_operator_node = try_calloc(1, sizeof(t_ast));
+	ast_operator_node->type = type;
+	ast_operator_node->left_hand = left_hand;
+	ast_operator_node->right_hand = right_hand;
+	return (ast_operator_node);
 }
 
-t_ast *ast_init_node()
+t_ast	*ast_init_node()
 {
-	t_ast *node;
-	node = try_calloc(1,sizeof(t_ast));
-	return(node); 
+	t_ast	*node;
+
+	node = try_calloc(1, sizeof(t_ast));
+	return (node);
 }
 
-void ast_addback(t_ast **head, t_ast *new_node)
+void	ast_addback(t_ast **head, t_ast *new_node)
 {
-	t_ast *node;
+	t_ast	*node;
+
 	node = *head;
-
-	while(node != NULL)
+	while (node != NULL)
 	{
-		if(node -> next == NULL)
-			break;
-		node = node-> next;
+		if (node->next == NULL)
+			break ;
+		node = node->next;
 	}
-	if(node != NULL)
+	if (node != NULL)
 	{
-		node-> next = new_node;
-
-		new_node -> next = NULL;
-		new_node -> prev = node;
+		node->next = new_node;
+		new_node->next = NULL;
+		new_node->prev = node;
 	}
 	else
 	{
-		new_node -> next = NULL;
-		new_node -> prev = NULL;
+		new_node->next = NULL;
+		new_node->prev = NULL;
 		*head = new_node;
 	}
 }
 
-void ast_word_list(t_command **command_list, t_token *token)
+void	command_word_list(t_command **command_list, t_token *token)
 {
-	t_command *node;
-	node = ast_init_word_node(token);
-	add_back_word_list(command_list,node);
+	t_command	*word_node;
+
+	word_node = word_init_node(token);
+	add_back_word_list(command_list, node);
 }
 
-void ast_redirect_list(t_redirect **redirect_list, t_token **current_token, t_data *d)
+t_word_list	*word_init_node(t_token *token)
 {
-	t_token	*token = *current_token;
-	t_redirect *node;
+	t_word_list	*node;
 
-	node = ast_init_redirect_node(token);
-	add_back_redirect_list(redirect_list,node);
-	token = token -> next;
-	if(token == NULL || is_operetor(token))
+	node = try_calloc(1, sizeof(t_word_list));
+	node->word = try_strdup(token->word);
+	node->index = token->index;
+	node->type = token->type;
+	return (node);
+}
+
+void			word_list_addback(t_word **head, t_word *node);
+{
+	;
+}
+
+void	command_redirect_list(t_redirect **redirect_list,
+							t_token **current_token,
+							t_data *d)
+{
+	t_token		*token;
+	t_redirect	*node;
+
+	token = *current_token;
+	node = redirect_init_node(token);
+	add_back_redirect_list(redirect_list, node);
+	token = token->next;
+	if (token == NULL || is_operetor(token))
 		syntax_error();
 	node = ast_init_redirect_node(token);
-	add_back_redirect_list(redirect_list,node);
-	token = token -> next;
-	if(token == NULL || is_operetor(token))
+	add_back_redirect_list(redirect_list, node);
+	token = token->next;
+	if (token == NULL || is_operetor(token))
 		syntax_error();
 }
 
 //redirectのnode(<)
-t_redirect_list *ast_init_redirect_node(t_token *token);
+t_redirect_list	*redirect_init_node(t_token *token);
 {
 	t_redirect_list *node;
-	node = try_calloc(1,sizeof(t_redirect));
+	node = try_calloc(1, sizeof(t_redirect));
 	//t_redirectとtokenをstrcmpなどで比較する必要がある
-	set_redirect_type(token);//redirectタイプをsetする関数
-	return(node);
+	set_redirect_type(token); //redirectタイプをsetする関数
+	return (node);
 }
 
-t_word_list *ast_init_word_node(t_token *token)
-{
-	t_word_list *node;
-	node = try_calloc(1,sizeof(t_word_list));
-	node->word = try_strdup(token->word);
-	node->index = token->index;
-	node->type = token->type;
-	return(node);
-}
-
-void word_list_addback(t_word **head,t_word *node);
+void	redirect_list_addback(t_redirect **head, t_redirect *node)
 {
 	;
 }
 
 //redirectのnode(word)
 //これってなんだっけ？
-t_redirect_list *ast_init_redirect_word_node(t_redirect *node);
+t_redirect_list	*ast_init_redirect_word_node(t_redirect *node);
 {
 	t_redirect_list *node;
-	node = try_calloc(1,sizeof(t_redirect));
+	node = try_calloc(1, sizeof(t_redirect));
 	set_word(node->word);
-	set_redirect_type(token);//redirectタイプをsetする関数
-	return(node);
-}
-
-void redirect_list_addback(t_redirect **head,t_redirect *node)
-{
-	;
+	set_redirect_type(token); //redirectタイプをsetする関数
+	return (node);
 }
 
 //token のindexを使う処理に変更するかも
 t_token	*token_next(t_token **current_token)
 {
-	t_token	*token = *current_token->next;
+	t_token	*token;
+
+	token = *current_token->next;
 	if (token != NULL && is_quotation_closed(token) == false)
 	{
 		syntax_error();
@@ -190,29 +207,39 @@ t_token	*token_next(t_token **current_token)
 	return (token);
 }
 
- void * syntax_error(t_ast *left_node,t_ast *right_node)
- {
-	*ast_free_all_nodes(t_ast *left_hanf);
-	*ast_free_all_nodes(t_ast *right_right);
- }
+void	*syntax_error(t_ast *left_node, t_ast *right_node, t_data *d)
+{
+	put_error("syntax_error");
+	d->exit_status = hogehoge;
+	d->syntax_flag = 1;
+	call_heardoc();
+	*ast_free_all_nodes(t_ast * left_hanf);
+	*ast_free_all_nodes(t_ast * right_right);
+}
 
 void	*ast_free_all_nodes(t_ast *node)
 {
-	if(node != NULL && node-> left_hand != NULL)
-		ast_free_all_nodes(node-> left_hand);
-	if(node != NULL && node-> right_hand != NULL)
-		ast_free_all_nodes(node-> right_hand);
+	if (node != NULL && node->left_hand != NULL)
+		ast_free_all_nodes(node->left_hand);
+	if (node != NULL && node->right_hand != NULL)
+		ast_free_all_nodes(node->right_hand);
 	free(node);
 	return (NULL);
 }
 
-bool ast_is_opereter(e_ast_type type)
+bool	ast_is_opereter(t_token_type type)
 {
 	;
 }
 
-void ast_expect(t_token **current_token,char op) {
-  if (current_token->kind != WORD || current_token->word[0] != op)
-    error("'%c'ではありません", op);
-  current_token = current_token->next;
+bool	command_is_redirect(t_token_type type)
+{
+	;
+}
+
+void	ast_expect(t_token **current_token, char op)
+{
+	if (current_token->kind != WORD || current_token->word[0] != op)
+		error("'%c'ではありません", op);
+	current_token = current_token->next;
 }
--- includes/parse.h
+++ includes/parse.h
@@ -23,14 +23,14 @@
 
 typedef enum e_redirect_type
 {
-	REDIRECTING_INPUT, // <
-	REDIRECTING_OUTPUT,// >
-	APPENDING_OUTPUT,// >>
-	FILE,// redirect fil
-	HERE_DOCUMENTS, // <<
-	DELIMITER, // << delimitter(クウォートがない場合)
-	QUOTE_DELIMITER, // << delimitter(クウォートがある場合)
-}	t_redirect_type;
+	REDIRECTING_INPUT,  // <
+	REDIRECTING_OUTPUT, // >
+	APPENDING_OUTPUT,   // >>
+	FILE,               // redirect fil
+	HERE_DOCUMENTS,     // <<
+	DELIMITER,          // << delimitter(クウォートがない場合)
+	QUOTE_DELIMITER,    // << delimitter(クウォートがある場合)
+}						t_redirect_type;
 
 typedef enum e_ast_node_type
 {
@@ -38,32 +38,32 @@ typedef enum e_ast_node_type
 	LOGICAL_AND,
 	LOGICAL_OR,
 	COMMAND,
-}	t_ast_node_type;
+}						t_ast_node_type;
 
 typedef struct s_word_list
 {
 	char				*word;
-	size_t				index;//クォートが閉じるまで更新されない
+	size_t index; //クォートが閉じるまで更新されない
 	t_token_type		type;
 	struct s_word_list	*next;
-}	t_word_list;
+}						t_word_list;
 
 typedef struct s_redirect_list
 {
 	char				*word;
 	size_t				index;
 	t_redirect_type		type;
-	bool 	is_ambiguas_error;//初期値はfalse
+	bool is_ambiguas_error; //初期値はfalse
 	struct s_redirect	*next;
-}	t_redirect_list;
+}						t_redirect_list;
 
 typedef struct s_command
 {
-	t_word_list	*word_list;
-	t_redirect	*redirect_list;
-	int			fd;//どこにコマンドを出力するか(初期値はSTD OUT FILNE NO)
-	pid_t		pid;//子プロセスのIDを管理(初期値は-1)
-}	t_command;
+	t_word_list			*word_list;
+	t_redirect			*redirect_list;
+	int fd;    //どこにコマンドを出力するか(初期値はSTD OUT FILNE NO)
+	pid_t pid; //子プロセスのIDを管理(初期値は-1)
+}						t_command;
 
 typedef struct s_ast
 {
@@ -71,99 +71,89 @@ typedef struct s_ast
 	struct s_command	*command_list;
 	struct s_ast		*left_hand;
 	struct s_ast		*right_hand;
-}	t_ast;
-
+}						t_ast;
 
 //t_ast関連
-t_ast *parse(t_token **current_token, t_data *d);
-t_ast *ast_command(t_token *token,t_data *d);
-t_ast *ast_command_list(t_ast *node,t_token **current_token);
-t_ast *ast_operator(e_ast_node_type,t_ast *left_hand,t_ast *right_hand);
-t_ast *ast_init_node()
-void ast_addback(t_ast **head, t_ast *new_node);
+t_ast					*parse(t_token **current_token, t_data *d);
+t_ast					*ast_command_node(t_token *token, t_data *d);
+t_ast					*ast_command_list(t_ast *node, t_token **current_token);
+t_ast					*ast_operator_node(e_ast_node_type, t_ast *left_hand,
+							t_ast *right_hand);
+t_ast					*ast_init_node();
+void					ast_addback(t_ast **head, t_ast *new_node);
+void					ast_free_all_nodes(t_ast *node);
 
 //t_command関連
-void command_word_list(t_command command_list, t_token token);
-void redirect_list(t_redirect redirect_list, t_token **token_address,t_data *d);
+void	command_word_list(t_command command_list,
+						t_token **current_token);
+void	command_redirect_list(t_command command_list,
+							t_token **current_token,
+							t_data *d);
+bool					command_is_redirect(t_token type);
 
 //t_word_list関連
-t_word_list *word_list_init(t_token *token);
-void *word_list_addback(t_word_list **head,t_word *node);
+t_word_list				*word_list_init(t_token *token);
+void					*word_list_addback(t_word_list **head, t_word *node);
 
 //t_redirect関連
-t_redirect *redirect_init(t_token *token);
-void *redirect_list_addback(t_redirect **head,t_redirect *node);
+t_redirect				*redirect_init(t_token *token);
+void	*redirect_list_addback(t_redirect **head,
+							t_redirect *node);
 
 //error関連
-bool ast_is_operator(e_ast_node_type type);
-bool ast_is_redirect(t_token type);
-bool is_quotation_closed(t_token *token_adress);
-void ast_expect(t_token **token,char op);
-t_token	*token_next(t_token **token_address);
-void ast_free_all_nodes(t_ast *node);
-void * ast_syntax_error(t_ast *left_node,t_ast *right_node)
+bool					ast_is_operator(t_token_type type);
+bool					is_quotation_closed(t_token *token_adress);
+void					ast_expect(t_token **token, char op);
+t_token					*token_next(t_token **token_address);
+void	*ast_syntax_error(t_ast *left_node, t_ast *right_node)
 
-/* void* try_calloc(size_t nmemb,size_t size); */
+		/* void* try_calloc(size_t nmemb,size_t size); */
 
-ls infileC a b | ls
+		ls infileC a b
+		| ls
 
-ls infile a b << eof <Makefile > out >> apend | ls a b c d
+				ls infile a b
+				<< eof<Makefile> out
+			>> apend
+		| ls a b c d
 
-1 ls 2 infile 3> 4a 5b 6<< 7eof <Makefile | ls
-1ls 2infile 5b
-3> 4a << eof <Makefile
+			1 ls 2 infile 3 > 4a 5b 6 << 7eof <
+			Makefile
+		| ls 1ls 2infile 5b 3 > 4a << eof < Makefile
 
-cat
-infiile
-|
-ls
+			cat infiile
+		|
+		ls
 
-------------------------
+		-- ----------------------
 
-cmd1 | cmd2 || cmd3 | cmd4
-
-				|
-			||		cmd4
-		|		cmd3
-	cmd1	cmd2
-------------------------
-
-cmd1 | (cmd2 || cmd3) | cmd4
+		cmd1
+		| cmd2
+	|| cmd3 | cmd4
 
 		|
-	cmd1	|
-		||	   cmd4
-	cmd2  cmd3
-------------------------
+	|| cmd4 | cmd3 cmd1 cmd2-- ----------------------
 
-------------------------
-(a || b) && ls
+		cmd1
+		| (cmd2 || cmd3) | cmd4
+
+		| cmd1 |
+	|| cmd4 cmd2 cmd3-- ----------------------
+
+		------------------------(a || b) &&
+		ls
 
 		&&
-	||		ls
-a		b
-------------------------
+	|| ls a b-- ----------------------
 
+		| | command4 | comamnd3 command1 command2
 
-					|
-				|		command4
-		  |			comamnd3
-command1	command2
+		| comamnd3 command1 command2
 
+		| command1 command2
 
-		  |			comamnd3
-command1	command2
+			< infile
 
-
-		  |
-command1	command2
-
-<infile
-
-
-		  |
-command1	command2
-					comamnd3
-						command4
+		| command1 command2 comamnd3 command4
 
 #endif
