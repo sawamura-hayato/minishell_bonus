3c912f4c1b6745eceaecd1b4c498e762d8b6a58c
Tatsuki Terao <tterao@c1r11s3.42tokyo.jp>
2023-08-29 14:44:09 +0900

    library関数を整理　不備関数を削除

--- library/srcs/try/try_open.c
+++ library/srcs/try/try_open.c
@@ -6,7 +6,7 @@
 /*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 14:40:59 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/08/24 14:38:01 by tterao           ###   ########.fr       */
+/*   Updated: 2023/08/29 14:40:16 by tterao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "init.h"
-
-// try_open(open(), d)
-// 可変長配列実装するのがめんどくさいので、openした値を見て判断する
 
 int	try_open(int open_value, char *file, t_data *d)
 {
@@ -27,8 +24,3 @@ int	try_open(int open_value, char *file, t_data *d)
 	}
 	return (open_value);
 }
-
-
-// RETURN VALUES
-//      If successful, open() returns a non-negative integer, termed a file descriptor.  It returns -1 on failure, and sets errno to
-//      indicate the error.
