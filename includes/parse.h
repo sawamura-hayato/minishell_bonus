189a8bb295a0f222d90d2949411dc95278572eff
Your Name <you@example.com>
2023-08-31 13:00:26 +0900

    norm対応 parse.h tokenize.h

--- includes/parse.h
+++ includes/parse.h
@@ -6,7 +6,7 @@
 /*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 15:49:20 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/08/30 19:21:44 by tyamauch         ###   ########.fr       */
+/*   Updated: 2023/08/31 12:41:07 by tyamauch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,47 +75,43 @@ typedef struct s_ast
 }							t_ast;
 
 // t_ast関連
-t_ast						*parse(t_token **current_token, t_data *d);
-t_ast						*ast_command_node(t_token **current_token,
-								t_data *d);
-t_ast						*ast_command_list(t_ast *ast_command_node,
-								t_token **current_token, t_data *d);
-t_ast						*ast_operator_node(t_ast_node_type type,
-								t_ast *left_hand, t_ast *right_hand, t_data *d);
-t_ast						*ast_init_node(void);
-void						ast_addback(t_ast **head, t_ast *new_node);
-void						*ast_free_all_nodes(t_ast *node);
+t_ast			*parse(t_token **current_token, t_data *d);
+t_ast			*ast_command_node(t_token **current_token, t_data *d);
+t_ast			*ast_command_list(t_ast *ast_command_node,
+					t_token **current_token, t_data *d);
+t_ast			*ast_operator_node(t_ast_node_type type, t_ast *left_hand,
+					t_ast *right_hand, t_data *d);
+t_ast			*ast_init_node(void);
+void			ast_addback(t_ast **head, t_ast *new_node);
+void			*ast_free_all_nodes(t_ast *node);
 
-void						debug_print_ast(t_ast *node);
+void			debug_print_ast(t_ast *node);
 // t_command関連
-void						command_word_list(t_word_list **word_list,
-								t_token **current_token, t_data *d);
-void						command_redirect_list(t_redirect_list
-								**redirect_list,
-								t_token **current_token, t_data *d,
-								bool redirect_flag);
-bool						token_is_redirect(t_token_type type);
+void			command_word_list(t_word_list **word_list,
+					t_token **current_token, t_data *d);
+void			command_redirect_list(t_redirect_list **redirect_list,
+					t_token **current_token, t_data *d,
+					bool redirect_flag);
+bool			token_is_redirect(t_token_type type);
 
-void						redirect_set_type(t_redirect_list **head,
-								t_redirect_list *node, t_token *token);
-void						redirect_set_type_word(t_redirect_list *last_node,
-								t_redirect_list *node, t_token *token);
-t_redirect_list				*redirect_list_get_last_node(t_redirect_list
-								**head);
-bool						is_redirect_operator(t_redirect_list *node);
+void			redirect_set_type(t_redirect_list **head,
+					t_redirect_list *node, t_token *token);
+void			redirect_set_type_word(t_redirect_list *last_node,
+					t_redirect_list *node, t_token *token);
+t_redirect_list	*redirect_list_get_last_node(t_redirect_list **head);
+bool			is_redirect_operator(t_redirect_list *node);
 
 // error関連
-bool						ast_is_opereter(t_token_type type);
-bool						token_is_quotation(t_token *token);
+bool			ast_is_opereter(t_token_type type);
+bool			token_is_quotation(t_token *token);
 
-bool						token_is_quotation_closed(t_token *token);
-void						ast_expect(t_token **current_token,
-								t_data *d);
-t_token						*token_next(t_token **current_token, t_data *d);
-void						ast_syntax_error(t_data *d, t_token *token);
-t_ast_node_type				set_ast_node_type(t_token *token);
-void						*ast_free_node(t_ast *node);
-void						*ast_free_right_left_nodes(t_ast *left_node,
-								t_ast *right_node);
+bool			token_is_quotation_closed(t_token *token);
+void			ast_expect(t_token **current_token, t_data *d);
+t_token			*token_next(t_token **current_token, t_data *d);
+void			ast_syntax_error(t_data *d, t_token *token);
+t_ast_node_type	set_ast_node_type(t_token *token);
+void			*ast_free_node(t_ast *node);
+void			*ast_free_right_left_nodes(t_ast *left_node,
+					t_ast *right_node);
 
 #endif
--- includes/tokenize.h
+++ includes/tokenize.h
@@ -11,18 +11,17 @@
 /* ************************************************************************** */
 
 #ifndef TOKENIZE_H
-#define TOKENIZE_H
+# define TOKENIZE_H
 
-#include <stdbool.h>
-#include <stddef.h>
-#include "library.h"
+# include "library.h"
+# include <stdbool.h>
+# include <stddef.h>
+# include <stdio.h>
 
-#include <stdio.h>
-
-#define DEFAULT_CHAR_TYPE (0)
-#define IS_SUBSTITUTED (1)
-#define IS_SINGLE_QUOTED (2)
-#define IS_DOUBLE_QUOTED (3)
+# define DEFAULT_CHAR_TYPE (0)
+# define IS_SUBSTITUTED (1)
+# define IS_SINGLE_QUOTED (2)
+# define IS_DOUBLE_QUOTED (3)
 
 typedef enum e_token_type
 {
@@ -33,14 +32,14 @@ typedef enum e_token_type
 	TK_LOGICAL_AND,
 	TK_OPEN_PARENTHESIS,
 	TK_CLOSE_PARENTHESIS
-} t_token_type;
+}					t_token_type;
 
 typedef enum e_quote
 {
 	DEFAULT,
 	SINGLE_QUOTE_FLAG,
 	DOUBLE_QUOTE_FLAG
-} t_quote;
+}					t_quote;
 
 typedef struct s_token
 {
@@ -48,39 +47,39 @@ typedef struct s_token
 	char			*type;
 	t_token_type	tk_type;
 	struct s_token	*next;
-} t_token;
+}					t_token;
 
 // tokenize.c
-t_token *tokenize(const char *line);
+t_token				*tokenize(const char *line);
 
 // get_current_word_size.c
-size_t token_get_current_word_size(char *line);
-size_t token_get_operator_size(char *line);
-size_t token_get_redirect_list_size(char *line);
-size_t token_get_special_word_size(char *line);
+size_t				token_get_current_word_size(char *line);
+size_t				token_get_operator_size(char *line);
+size_t				token_get_redirect_list_size(char *line);
+size_t				token_get_special_word_size(char *line);
 
 // get_current_token.c
-char *token_get_current_word(char *line, size_t size);
-void token_get_current_token(t_token **head, char **line);
-char	*token_get_type_word(char *word, bool is_expand);
+char				*token_get_current_word(char *line, size_t size);
+void				token_get_current_token(t_token **head, char **line);
+char				*token_get_type_word(char *word, bool is_expand);
 
 // set_flag.c
-t_quote token_set_flag_quote(char quote);
-t_token_type token_set_flag_token(char *line);
+t_quote				token_set_flag_quote(char quote);
+t_token_type		token_set_flag_token(char *line);
 
 // linear_linked_token.c
-t_token *create_token(char *word, t_token_type token_type);
-void token_addback(t_token **head, t_token *new_token);
-t_token *token_end(void);
-void token_free_all_tokens(t_token *token_list);
-void debug_print_token(t_token *token_list);
+t_token				*create_token(char *word, t_token_type token_type);
+void				token_addback(t_token **head, t_token *new_token);
+t_token				*token_end(void);
+void				token_free_all_tokens(t_token *token_list);
+void				debug_print_token(t_token *token_list);
 
 // library
-int ft_isspace(char c);
-bool ft_is_special_char(char c);
-size_t ft_strlen(const char *str);
+int					ft_isspace(char c);
+bool				ft_is_special_char(char c);
+size_t				ft_strlen(const char *str);
 
-void *try_malloc(size_t size);
-void *try_calloc(size_t count, size_t size);
+void				*try_malloc(size_t size);
+void				*try_calloc(size_t count, size_t size);
 
 #endif
