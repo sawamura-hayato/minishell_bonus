6a8d5fb9a111a659a41e68027de60d47ec07e1bf
Your Name <you@example.com>
2023-09-07 19:00:25 +0900

    syntax_flag trueを削除

--- srcs/parse/command_redirect_list.c
+++ srcs/parse/command_redirect_list.c
@@ -6,7 +6,7 @@
 /*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 19:37:37 by tterao            #+#    #+#             */
-/*   Updated: 2023/09/07 17:48:10 by tterao           ###   ########.fr       */
+/*   Updated: 2023/09/07 18:58:26 by tyamauch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,20 +75,11 @@ void	command_redirect_list(t_redirect_list **head,
 
 	token = *current_token;
 	if (token_is_quotation_closed(token) == false)
-	{
-		d->syntax_flag = true;
 		ast_syntax_error(d, token);
-	}
 	node = redirect_init_node(head, token, redirect_flag);
 	if (redirect_flag == false && token->next == NULL)
-	{
-		d->syntax_flag = true;
 		ast_syntax_error(d, NULL);
-	}
 	else if (redirect_flag == true && node->re_type == (t_redirect_type)(-1))
-	{
-		d->syntax_flag = true;
 		ast_syntax_error(d, token);
-	}
 	redirect_list_addback(head, node);
 }
