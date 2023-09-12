97032bccbb262e502b1b5247562fd36c87f29857
Tatsuki Terao <tterao@c1r24s10.42tokyo.jp>
2023-09-12 17:14:08 +0900

    signal終了ステータスを修正

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsawamur <hsawamur@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 13:02:01 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/09/11 15:21:18 by hsawamur         ###   ########.fr       */
+/*   Updated: 2023/09/12 17:10:34 by tterao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,15 @@ void	command_execution(t_ast_l1 *node, t_data *d)
 	if (node->left_hand != NULL)
 		command_execution(node->left_hand, d);
 	if (node->type != AST_COMMAND && g_signal_num == SIGINT)
-	{
-		d->exit_status = SIGINT_EXITSTATUS;
 		return ;
-	}
 	else if (
 		(node->type == AST_LOGICAL_AND && d->exit_status == EXIT_SUCCESS)
 		|| (node->type == AST_LOGICAL_OR && d->exit_status != EXIT_SUCCESS))
+	{
 		command_execution(node->right_hand, d);
+		if (g_signal_num == SIGINT)
+			d->exit_status = SIGINT_EXITSTATUS;
+	}
 	if (node->type == AST_COMMAND)
 	{
 		try_dup2(d->dupped_stdinfd, STDIN_FILENO, d);
