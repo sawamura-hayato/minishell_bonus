398a4a7bb9dfb8f3b04f071ca6ceb4d84fa9c252
Tatsuki Terao <tterao@c1r23s7.42tokyo.jp>
2023-09-15 18:42:56 +0900

    ignore signalを追加

--- srcs/main.c
+++ srcs/main.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsawamur <hsawamur@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 14:11:21 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/09/08 15:39:16 by hsawamur         ###   ########.fr       */
+/*   Updated: 2023/09/15 18:41:29 by tterao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "init.h"
+
+void	ignore_signal(t_data *d);
 
 int	main(void)
 {
@@ -19,6 +21,7 @@ int	main(void)
 	extern const char	**environ;
 
 	d.exit_status = 0;
+	ignore_signal(&d);
 	envs_init(environ, &d);
 	read_eval_print_loop(&d);
 	return (0);
