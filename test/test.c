9234b7d8e684feb0d5ad7e696da2636e52f392b5
Sawa <sunzecun@gmail.com>
2023-09-20 14:37:03 +0900

    テストケース追加

---
+++ test/test.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    // execve("/usr/bin/valgrind", (char *[]){"/usr/bin/valgrind", "./minishell", NULL}, (char *[]){"", NULL});
+    execve("./minishell", (char *[]){"./minishell",  NULL}, (char *[]){"", NULL});
+    // perror("");
+}
