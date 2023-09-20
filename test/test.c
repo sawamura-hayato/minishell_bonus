4995ad916a6a8f97232b1323701fcf8118651e05
Sawa <sunzecun@gmail.com>
2023-09-20 14:39:17 +0900

    実行ファイルからのテストケース

--- test/test.c
+++ test/test.c
@@ -4,6 +4,6 @@
 int main()
 {
     // execve("/usr/bin/valgrind", (char *[]){"/usr/bin/valgrind", "./minishell", NULL}, (char *[]){"", NULL});
-    execve("./minishell", (char *[]){"./minishell",  NULL}, (char *[]){"", NULL});
+    execve("../minishell", (char *[]){"../minishell",  NULL}, (char *[]){"a", NULL});
     // perror("");
 }
