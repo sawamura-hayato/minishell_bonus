5092230a8609dbb3a95d7f875bae60978ee712e6
Hayato Sawamura <hsawamur@c1r24s1.42tokyo.jp>
2023-09-26 13:08:58 +0900

    value設定する時の一部の記号をエラー出力するようにした

--- srcs/builtin/export/export_is_error.c
+++ srcs/builtin/export/export_is_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export_is_error.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsawamur <hsawamur@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: hsawamur <hsawamur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 15:27:37 by tterao            #+#    #+#             */
-/*   Updated: 2023/08/15 17:13:51 by tterao           ###   ########.fr       */
+/*   Updated: 2023/09/26 13:06:29 by hsawamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static bool	export_is_invalid_key(char *str, t_data *d)
 	}
 	while (*str != '=' && *str != '+' && *str != '\0')
 	{
-		if (export_is_symbol(*str))
+		if (export_is_symbol(*str) || *str == ' ' || *str == '	')
 		{
 			export_invalid_identifier(str, d);
 			return (true);
